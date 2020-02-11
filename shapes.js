c4a0b63faea6b68e98743ff9604a732539cb65f7
Nick Mc <nbmc@protonmail.com>
2020-02-11 12:30:10 -0600

    basic circle functionality.

--- shapes.js
+++ shapes.js
@@ -1,25 +1,49 @@
+console.log("Loading..");
+
 const CANVAS = document.getElementById('canvas');
 const BTN_SQUARE = document.getElementById('square-btn');
 
-class Square {
+class Shape {
     constructor(h) {
-        this.h = h;
-        this.offset = offset;
-        this.square = document.createElement('div');
-        this.square.classList.add('square');
-        this.square.style.height = (h + 'px');
-        this.square.style.width = (h + 'px');
-        this.square.style.top = offset(h);
-        this.square.style.left = offset(h);
-        CANVAS.appendChild(this.square);
+        this.h = this.inputCheck(h);
+        this.offsetV = this.offset(this.h);
+        this.offsetH = this.offset(this.h);
+        this.shape = document.createElement('div');
+        this.shape.style.height = (this.h + 'px');
+        this.shape.style.width = (this.h + 'px');
+        this.shape.style.top = this.offsetV;
+        this.shape.style.left = this.offsetH;
+    }
+    inputCheck(x) {
+        if ((x.length === 0) || (Number.isNaN(x) === true)) {
+            return 100;
+        } else {
+            return x;
+        }
+    }
+    offset(x) {
+        return (Math.abs((Math.floor(Math.random() * 600)) - x - 4) + 'px');
     }
 }
-function offset(x) {
-    let o = Math.abs((Math.floor(Math.random() * 600)) - x - 4);
-    console.log(o);
-    return o + 'px';
+class Circle extends Shape {
+    constructor(h) {
+        super(h);
+        this.shape.classList.add('circle');
+        CANVAS.appendChild(this.shape);
+    }
 }
+class Square extends Shape {
+    constructor(h) {
+        super(h);
+        this.shape.classList.add('square');
+        CANVAS.appendChild(this.shape);
+    }
+}
+
 BTN_SQUARE.addEventListener('click', () => {
     let h = document.getElementById('square-input').value;
     new Square(h);
+    new Circle(h);
 });
+
+console.log("Loaded.");
