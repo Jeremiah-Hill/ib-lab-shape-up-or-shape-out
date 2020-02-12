4ae0ed37ed394c16a8fe9920d96050eee1eaa7ff
Nick Mc <nbmc@protonmail.com>
2020-02-11 21:18:02 -0600

    refactoring existing functionality.

--- shapes.js
+++ shapes.js
@@ -1,18 +1,59 @@
 console.log("Loading..");
 
 const CANVAS = document.getElementById('canvas');
-const BTN_SQUARE = document.getElementById('square-btn');
+
+// circle listener.
+document.getElementById('circle-btn').addEventListener('click', () => {
+    let r = document.getElementById('circle-radius').value;
+    new Circle(r);
+});
+// square listener.
+document.getElementById('square-btn').addEventListener('click', () => {
+    let l = document.getElementById('square-length').value;
+    new Square(l);
+});
 
 class Shape {
-    constructor(h) {
-        this.h = this.inputCheck(h);
-        this.offsetV = this.offset(this.h);
-        this.offsetH = this.offset(this.h);
+    constructor() {
         this.shape = document.createElement('div');
-        this.shape.style.height = (this.h + 'px');
-        this.shape.style.width = (this.h + 'px');
-        this.shape.style.top = this.offsetV;
-        this.shape.style.left = this.offsetH;
+    }
+    
+    shapeAppend() {
+        CANVAS.appendChild(this.shape);
+    }
+}
+class Circle extends Shape {
+    constructor(r) {
+        super();
+        this.r = this.inputCheck(r);
+        this.shape.style.height = (this.r * 2 + 'px');
+        this.shape.style.width = (this.r * 2 + 'px');
+        this.shape.style.top = this.offset(this.r);
+        this.shape.style.left = this.offset(this.r);
+        this.shape.classList.add('circle');
+        this.shapeAppend();
+    }
+    inputCheck(x) {
+        if ((x.length === 0) || (Number.isNaN(x) === true)) {
+            return 50;
+        } else {
+            return x;
+        }
+    }
+    offset(x) {
+        return Math.abs((Math.floor(Math.random() * (x * 9))) - x - 4) + 'px';
+    }
+}
+class Square extends Shape {
+    constructor(l) {
+        super();
+        this.l = this.inputCheck(l);
+        this.shape.style.height = (this.l + 'px');
+        this.shape.style.width = (this.l + 'px');
+        this.shape.style.top = this.offset(this.l);
+        this.shape.style.left = this.offset(this.l);
+        this.shape.classList.add('square');
+        this.shapeAppend();
     }
     inputCheck(x) {
         if ((x.length === 0) || (Number.isNaN(x) === true)) {
@@ -22,28 +63,8 @@ class Shape {
         }
     }
     offset(x) {
-        return (Math.abs((Math.floor(Math.random() * 600)) - x - 4) + 'px');
+        return Math.abs((Math.floor(Math.random() * (x * 6))) - x - 4) + 'px';
     }
 }
-class Circle extends Shape {
-    constructor(h) {
-        super(h);
-        this.shape.classList.add('circle');
-        CANVAS.appendChild(this.shape);
-    }
-}
-class Square extends Shape {
-    constructor(h) {
-        super(h);
-        this.shape.classList.add('square');
-        CANVAS.appendChild(this.shape);
-    }
-}
-
-BTN_SQUARE.addEventListener('click', () => {
-    let h = document.getElementById('square-input').value;
-    new Square(h);
-    new Circle(h);
-});
 
 console.log("Loaded.");
