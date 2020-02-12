afd4d8abfb1f50d765ab33bc178c4908457a4415
Nick Mc <nbmc@protonmail.com>
2020-02-12 09:41:00 -0600

    moving inputCheck() to super class.

--- scripts.js
+++ scripts.js
@@ -17,7 +17,36 @@ class Shape {
     constructor() {
         this.shape = document.createElement('div');
     }
-    
+    inputCheck(name, x) {
+        let def;
+        switch (name) {
+            case 'circle':
+                def = 50;
+                break;
+            case 'square':
+                def = 100;
+                break;
+            case 'triangle':
+                def = 100;
+                break;
+            case 'rectangle':
+                def = 70;
+                break;
+        }
+        if ((x.length === 0) || (Number.isNaN(x) === true)) {
+            return def;
+        } else {
+            return x;
+        }
+    }
+    offset(x) {
+        let y = Math.floor(Math.random() * 601 - x) - 4;
+        if (y < 0) {
+            let y = Math.floor(Math.random() * 601 - x) - 4;
+        } else {
+            return y;
+        }
+    }
     shapeAppend() {
         CANVAS.appendChild(this.shape);
     }
@@ -25,45 +54,30 @@ class Shape {
 class Circle extends Shape {
     constructor(r) {
         super();
-        this.r = this.inputCheck(r);
-        this.shape.style.height = (this.r * 2 + 'px');
-        this.shape.style.width = (this.r * 2 + 'px');
-        this.shape.style.top = this.offset(this.r);
-        this.shape.style.left = this.offset(this.r);
+        this.name = 'circle';
+        this.r = this.inputCheck(this.name, r);
+        this.h = this.r * 2;
+        this.shape.style.height = this.h + 'px';
+        this.w = this.r * 2;
+        this.shape.style.width = this.w + 'px';
+        this.shape.style.top = this.offset(this.h) + 'px';
+        this.shape.style.left = this.offset(this.w) + 'px';
         this.shape.classList.add('circle');
         this.shapeAppend();
     }
-    inputCheck(x) {
-        if ((x.length === 0) || (Number.isNaN(x) === true)) {
-            return 50;
-        } else {
-            return x;
-        }
-    }
-    offset(x) {
-        return Math.abs((Math.floor(Math.random() * (x * 9))) - x - 4) + 'px';
-    }
+    
 }
 class Square extends Shape {
     constructor(l) {
         super();
-        this.l = this.inputCheck(l);
+        this.name = 'square';
+        this.l = this.inputCheck(this.name, l);
         this.shape.style.height = (this.l + 'px');
         this.shape.style.width = (this.l + 'px');
-        this.shape.style.top = this.offset(this.l);
-        this.shape.style.left = this.offset(this.l);
+        this.shape.style.top = this.offset(this.l) + 'px';
+        this.shape.style.left = this.offset(this.l) + 'px';
         this.shape.classList.add('square');
         this.shapeAppend();
-    }
-    inputCheck(x) {
-        if ((x.length === 0) || (Number.isNaN(x) === true)) {
-            return 100;
-        } else {
-            return x;
-        }
-    }
-    offset(x) {
-        return Math.abs((Math.floor(Math.random() * (x * 6))) - x - 4) + 'px';
     }
 }
 class Rectangle extends Shape {
